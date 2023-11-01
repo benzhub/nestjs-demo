f7325372c89c839d66870bb46f0f330609b6e701
lenrich <lenrich1403@gmail.com>
2023-11-02 05:23:19 +0800

    53-005 Error Handling_ Username Conflicts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MongoRepository } from 'typeorm';
 import { User } from './user.entity';
@@ -16,7 +20,15 @@ export class AuthService {
     const { username, password } = authCredentialsDto;
 
     const user = this.usersRepository.create({ username, password });
-    await this.usersRepository.save(user);
-    // return user;
+    try {
+      await this.usersRepository.save(user);
+    } catch (error) {
+      console.log(error.code); // 23505 => 這是用戶重複的Error代碼
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists!');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
