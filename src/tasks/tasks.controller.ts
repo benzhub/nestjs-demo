6a5a25509b8df3dd4ae2233a476709b3a5203d8d
lenrich <lenrich1403@gmail.com>
2023-11-02 06:31:31 +0800

    62-014 Guarding the Tasks Routes(在controller中加入jwt token請求路由)

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,15 +7,17 @@ import {
   Delete,
   Patch,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-// import { TaskStatus } from './task-status.enum';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks.filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import { Task } from './task.entity';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('tasks') // tasks also as router url
+@UseGuards(AuthGuard())
 export class TasksController {
   constructor(private tasksService: TasksService) {}
   @Get()
